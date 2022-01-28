f2c01cb088869ed02f07f9aad691155c5d81ac48
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-28 11:09:57 +0530

    styles seperated

---
+++ styles.ts
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  sectionContainer: {
+     flex: 1,
+justifyContent: 'center',
+      alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  container: {
+      flex: 1,
+
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center"
+      },
+  button: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      padding: 10,
+      marginBottom: 10
+    },
+    nametag: {
+    backgroundColor:'#fcfcfc',
+    padding: -23
+    },
+
+});
+export default styles;
