2e873115853b24f372f3e087240fbd4dd2f91828
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-29 12:59:29 +0530

    added homepage theme

---
+++ HomePage.tsx
@@ -0,0 +1,15 @@
+import homestyle from './homestyle'
+import React, { Component }  from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+  useColorScheme,
+  View,
+  ImageBackground,
+  TextInput,
+
+} from 'react-native';
+const image = { uri: "https://cdn.discordapp.com/avatars/619474506381000706/ea9046cae5081106339cb15e5f150c52.png?size=4096" };
---
+++ homestyle.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+const homestyle=StyleSheet.create({
+image: {
+        flex: 1,
+        justifyContent: "center"
+      },
+})
+export default homestyle;
