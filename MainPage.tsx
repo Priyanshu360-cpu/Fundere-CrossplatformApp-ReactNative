6c2d8f32decac091967569cdb75f9b3a68e0d7b6
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-23 08:54:29 +0530

    MainPage Seperated

---
+++ MainPage.tsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import type {Node} from 'react';
+import styles from './styles';
+export default class Homepage extends React.Component{
+    render(){
+        return(
+            <Text >HomePage Testing</Text>
+        )
+    }
+}
