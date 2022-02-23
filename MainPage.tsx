4937c0b1091828a4288a2f861634720726678754
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-23 09:09:40 +0530

    Updated

--- MainPage.tsx
+++ MainPage.tsx
@@ -7,7 +7,7 @@ import {
 export default class Homepage extends React.Component{
     render(){
         return(
-            <Text>HomePage Testing</Text>
+            <Text> HomePage Testing</Text>
         )
     }
 }
