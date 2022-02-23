0f62cff3179be74dec957e02cef24b50d4bf386f
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-23 08:54:03 +0530

    Index size reduced

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 import React, { Component }  from 'react';
 import type {Node} from 'react';
 import styles from './styles';
+import MainPage from './MainPage';
 import Placeholder from './holders';
 import HomepageScreen from './HomePage';
 import {
@@ -16,6 +17,7 @@ import {
   TextInput,
 
 } from 'react-native';
+import Login from './login';
 import { WebView } from 'react-native-webview';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -26,10 +28,11 @@ const MyStack = () => {
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={App}
+          component={Login}
           options={{ title: 'Welcome' }}
         />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="MainPage" component={MainPage}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -38,22 +41,8 @@ const ProfileScreen = ({ navigation, route }) => {
   return <Text>This is {route.params.name}'s profile</Text>;
 };
 const image = { uri: "https://cdn.discordapp.com/avatars/619474506381000706/ea9046cae5081106339cb15e5f150c52.png?size=4096" };
-const bgimg = ({title}): Node=> {
-  return(
-    <View style={styles.sectionContainer}>
-    <Text>
-       Heyo
-       {title}</Text>
-       </View>
-  )
-}
-const Greeting = (props) => {
-  return (
-    <View style={styles.center}>
-      <Text>Hello {props.name}!</Text>
-    </View>
-  );
-}
+
+
 const UselessTextInput = (props) => {
   return (
     <TextInput
@@ -73,82 +62,9 @@ return(<UselessTextInput
                         secureTextEntry={true}
                         style={styles.nametag}
                       />)
-
 }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
- class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  onSign = ({ navigation }) => {
-    const { navigate } = this.props.navigation;
-    navigate('Profile', { name: 'Jane' })
-  }
-
- render() {
-    return (
-      <View style={styles.container}>
- <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-<Placeholder></Placeholder>
-<Password></Password>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-                 style={styles.button}
-                 onPress={this.onSign}
-                >
-                 <Text>New? Sign In</Text>
-                </TouchableOpacity>
-        <View>
-          <Text>
-            You clicked { this.state.count } times
-
-          </Text>
-
-        </View>
-        </ImageBackground>
-      </View>
-
-    )
-  }
-}
 export default MyStack;
 
 
