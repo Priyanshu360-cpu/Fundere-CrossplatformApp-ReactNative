35a87b5f36e68b8185010c96af625c31a86a8ad8
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-23 08:57:34 +0530

    Bugs resolved

--- MainPage.tsx
+++ MainPage.tsx
@@ -1,10 +1,13 @@
 import React,{Component} from 'react';
 import type {Node} from 'react';
 import styles from './styles';
+import {
+    Text
+  } from 'react-native';
 export default class Homepage extends React.Component{
     render(){
         return(
-            <Text >HomePage Testing</Text>
+            <Text>HomePage Testing</Text>
         )
     }
 }
