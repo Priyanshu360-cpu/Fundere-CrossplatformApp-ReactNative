ed61e195cd9fa2e0ac7e479e61dab13639bbabe8
PRIYANSHU <tpriyanshu775@gmail.com>
2022-03-15 23:14:50 +0530

    animation added

---
+++ Welcanimate.tsx
@@ -0,0 +1,26 @@
+import homestyle from './homestyle'
+import React, { Component }  from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    TouchableOpacity,
+    Text,
+    useColorScheme,
+    View,
+    ImageBackground,
+    TextInput,
+    Animated
+  
+  } from 'react-native';
+  import * as Animatable from "react-native-animatable";
+class Welcanimate extends Component{
+    render(){
+        return(
+           <SafeAreaView>
+
+           </SafeAreaView>
+        )
+    }
+}
+export default Welcanimate;
