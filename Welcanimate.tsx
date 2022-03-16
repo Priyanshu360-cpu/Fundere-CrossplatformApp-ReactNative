95a52bc10d74c2036b91d3e2b54ff2daf99eeb15
PRIYANSHU <tpriyanshu775@gmail.com>
2022-03-17 00:58:31 +0530

    added animated page

--- Welcanimate.tsx
+++ Welcanimate.tsx
@@ -14,12 +14,27 @@ import {
   
   } from 'react-native';
   import * as Animatable from "react-native-animatable";
+  import styles from './animated';
 class Welcanimate extends Component{
+    componentWillMount = () => {
+        this.animatedWidth = new Animated.Value(50)
+        this.animatedHeight = new Animated.Value(100)
+     }
+     animatedBox = () => {
+        Animated.timing(this.animatedWidth, {
+           toValue: 200,
+           duration: 1000
+        }).start()
+        Animated.timing(this.animatedHeight, {
+           toValue: 500,
+           duration: 500
+        }).start()
+    }
     render(){
         return(
-           <SafeAreaView>
-
-           </SafeAreaView>
+            <TouchableOpacity style = {styles.container} onPress = {this.animatedBox}>
+            <Animated.View style = {[styles.box]}/>
+         </TouchableOpacity>
         )
     }
 }
