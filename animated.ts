6aea14980ec03a407d14da0751db26a30d05ffe5
PRIYANSHU <tpriyanshu775@gmail.com>
2022-03-17 00:58:46 +0530

    stylesheet for animated page

---
+++ animated.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+       justifyContent: 'center',
+       alignItems: 'center'
+    },
+    box: {
+       backgroundColor: 'blue',
+       width: 50,
+       height: 100
+    }
+ })
+ export default styles;
